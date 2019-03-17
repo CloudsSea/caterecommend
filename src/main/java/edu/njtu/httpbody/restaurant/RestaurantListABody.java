e4e1b18df943126ae95c5d8de0d9be7d2418d64c
CloudsSea <374737719@qq.com>
2019-03-17 14:10:34 +0800

    fix some body

--- src/main/java/edu/njtu/httpbody/restaurant/RestaurantListABody.java
+++ src/main/java/edu/njtu/httpbody/restaurant/RestaurantListABody.java
@@ -1,7 +1,8 @@
 package edu.njtu.httpbody.restaurant;
 
+import edu.njtu.httpbody.PageABody;
 import edu.njtu.httpbody.PageDBody;
 
-public class RestaurantListABody extends PageDBody {
+public class RestaurantListABody extends PageABody {
     private String optType;
 }
--- src/main/java/edu/njtu/httpbody/review/ReviewListABody.java
+++ src/main/java/edu/njtu/httpbody/review/ReviewListABody.java
@@ -1,7 +1,8 @@
 package edu.njtu.httpbody.review;
 
+import edu.njtu.httpbody.PageABody;
 import edu.njtu.httpbody.PageDBody;
 
-public class ReviewListABody extends PageDBody {
+public class ReviewListABody extends PageABody {
     private String optType;
 }
