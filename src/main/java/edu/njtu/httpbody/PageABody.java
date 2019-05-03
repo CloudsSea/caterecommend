23e06f6bd438b82111090fd77a062e9a457d96cd
CloudsSea <374737719@qq.com>
2019-05-03 19:32:03 +0800

    update PageABody to fit swagger's require

--- src/main/java/edu/njtu/httpbody/PageABody.java
+++ src/main/java/edu/njtu/httpbody/PageABody.java
@@ -1,7 +1,13 @@
 package edu.njtu.httpbody;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
 public class PageABody {
+    @ApiModelProperty(value="页码",required=true,dataType="Integer",name="pageNumber",example="1",allowableValues = "1,2")
     private Integer pageNumber;
+    @ApiModelProperty(value="单页大小",required=true,dataType="Integer",name="pageSize",example="10",allowableValues = "10,20")
     private Integer pageSize;
 
     public Integer getPageNumber() {
