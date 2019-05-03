71dd8b562a0bd091b01c84c176ed9b8ec5b97986
CloudsSea <374737719@qq.com>
2019-05-03 17:21:48 +0800

    some js for page of web module

--- web/src/index.js
+++ web/src/index.js
@@ -10,6 +10,10 @@ import {RouterStore, syncHistoryWithStore} from "mobx-react-router";
 import createHashHistory from "history/createHashHistory"
 import Test from "./component/Test"
 import ToDoapp from "./component/ToDoapp";
+import Album from "./component/Album";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import AlertDismissible from "./component/AlertDismissible";
 
 
 let rootStore = {}
@@ -23,14 +27,16 @@ routerStore['routing'] = routerStore
 ReactDOM.render(
     <Provider {...rootStore}>
         <Router history={history}>
-            <p>here is the menu</p>
+            <Route component={Header}/>
             <Switch>
                 <Route path={"/test"} component={Test}/>
                 <Route path={"/bs"} component={ToDoapp}/>
+                <Route path={"/album"} component={Album}/>
                 <Route path={"/"} component={App}/>
             </Switch>
-
+            <Route component={Footer}/>
         </Router>
+
 
     </Provider>, document.getElementById('root'));
 
